a37dcc02023c421bbff54f644327983275098511
medhatem <medhatem@hotmail.fr>
2019-10-17 16:09:43 -0400

    Create of the basic android project

---
+++ HockeyLite/app/src/main/java/ca/hatem/hockeylite/MainActivity.kt
@@ -0,0 +1,12 @@
+package ca.hatem.hockeylite
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
